e2ab44b9e9f2395bbbf3e90c3f5d135f4af8f2eb
jeffersonguanabara <jefferson.guanabara@dce.ufpb.br>
2020-08-06 21:49:02 -0300

    atualizando a conexão com banco de dados

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,13 +2,15 @@ import knex from 'knex';
 
 const connection = knex({
     client: 'pg',
-    version: '8.3.0',
-    connection: {
-        host : '127.0.0.1',
-        user : 'postgres',
-        password : 'root',
-        database : 'ibm_cultos'
-    }
+    // version: '8.3.0',
+    // connection: {
+    //     host : '127.0.0.1',
+    //     user : 'postgres',
+    //     password : 'root',
+    //     database : 'ibm_cultos'
+    // }
+    connection: process.env.DATABASE_URL,
+    searchPath: ['knex', 'public'],
 });
 
 export default connection;
